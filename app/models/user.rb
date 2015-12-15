0d39004730f49c2449d3ccbb4fcf5ef9512019f0
Clifton Regis <cregisexcel@yahoo.com>
2015-12-16 02:25:29 +0430

    add slugs to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   validates_presence_of :password, on: :create, length: {minimum: 4}
   validates :username, presence: true, uniqueness: true
+
+  before_save :generate_slug
+
+  def to_param
+    self.slug
+  end
+
+  def generate_slug
+    self.slug = self.username.gsub(" ","-").downcase
+  end
 end
